46db56a661d04edbfdb6eba9fbddaa2ff051db6c
Isaiah411013 <Archangel_r320@hotmail.com>
2022-08-26 18:23:38 -0700

    added some pseudocode

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  console.log( "You clicked the botton")
+  
+  // 1. prompts for password criteria
+  
+
+  // 2. select which criteria to include in password
+
+  
+  // 3. length of at least 8 characters,no more than 128 characters
+
+  
+  // 4. confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+  
+  // 5. input should be validated and at least one character type should be selected
+
+  
+  // 6. password is generated that matches the selected criteria
+
+
+  // 7.  password written to the page
+
+  return "working on it"
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +39,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
